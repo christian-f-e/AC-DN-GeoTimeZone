278d6db816eaaa259c394ddf8513c490721cbd19
Christian Abel Fernandez <christian.fernandez@ext.arcacontal.com>
2024-07-02 13:43:28 -0600

    Add project files.

---
+++ API/Controllers/GeoTimeZoneController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GeoTimeZoneController : ControllerBase
+    {
+
+        [HttpGet("")]
+        public async Task<IActionResult> GetGeoTimeZone(string latitude, string longitude)
+        {
+            var client = new RestClient($"https://api.geotimezone.com");
+            var request = new RestRequest("/public/timezone", Method.Get);
+
+            request.AddParameter("latitude", latitude);
+            request.AddParameter("longitude", longitude);
+
+            var response = client.Execute(request);
+
+            return Ok(response.Content);
+        }
+
+    }
+}
